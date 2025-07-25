967e5f509b183d62cb9ad9a3214bf0f4a778f027
pooja virani <pooja1522@gmail.com>
2025-07-25 15:55:44 +0530

    Update setup of tailwind to work with vite

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export let config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
